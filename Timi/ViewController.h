7f834c1dbc08040ab305f7dc02f278efb16317a4
ffm <fengfuming2007@126.com>
2017-01-21 11:00:41 +0800

    Initial Commit

---
+++ Timi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Timi
+//
+//  Created by ffm on 17/1/21.
+//  Copyright © 2017年 ITPanda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Timi/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Timi
+//
+//  Created by ffm on 17/1/21.
+//  Copyright © 2017年 ITPanda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
